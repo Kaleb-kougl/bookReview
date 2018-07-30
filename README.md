a82f22e8c0b89e8ed3168efc9aef5244fa78485c
Kaleb-kougl <kalebkougl@gmail.com>
2018-07-30 17:06:10 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Book Review web application using Python, HTML5, and CSS3.
+
+Uses a Flask framework for server implementation, a postgreSQL database, and sqlalchemy as a sql toolkit.
+
+Made by Kaleb Kougl
+
+Import.py - A program to upload data to the sql database
+
+Application.py - The server implementation
+
+books.csv - The data that was to be uploaded to the sql database
---
+++ application.py
@@ -0,0 +1,166 @@
+import os
+
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+#@app.route("/", methods=["GET", "POST"])
+#def new():
+#    if session.get("notes") is None:
+#        session["notes"] = []
+#    if request.method == "POST":
+#        note = request.form.get("note")
+#        session["notes"].append(note)
+
+#    return render_template("notes.html", notes=session["notes"])
+
+@app.route("/", methods=["GET"])
+def landing():
+    if session.get("user") is None:
+        session["user"] = ""
+    return render_template("landingPage.html")
+
+
+@app.route("/success", methods=["POST"])
+def success():
+#registration functionality
+    email = request.form.get("email")
+    password = request.form.get("password")
+    firstName = request.form.get("firstName")
+    lastName = request.form.get("lastName")
+    if email is "" or password is "" or firstName is "" or lastName is "":
+        return render_template("error.html")
+    else:
+        db.execute("INSERT INTO users (email, password, firstName, lastName) VALUES (:email, :password, :firstName, :lastName)", {"email":email, "password":password, "firstName":firstName, "lastName":lastName})
+        db.commit()
+        return render_template("success.html", email=email, password=password)
+
+
+@app.route("/logIn", methods=["GET", "POST"])
+def logIn():
+    if session.get("book_id") != None:
+        session["book_id"] = None
+
+    if session.get("user") is None:
+        session["user"] = ""
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = db.execute("SELECT * FROM users WHERE users.email=:email AND users.password=:password", {"email":email, "password":password}).fetchone()
+
+        if user is None:
+            loggedIn = False
+            user = "notFound"
+            return render_template("LogIn.html", loggedIn=loggedIn, user=user)
+
+        user = db.execute("SELECT id FROM users WHERE users.email=:email AND users.password=:password", {"email":email, "password":password}).fetchone()[0]
+        session["user"] = user
+        loggedIn = True
+        return render_template("logIn.html", loggedIn=loggedIn, user=email)
+    elif request.method == "GET":
+        loggedIn=False
+        user=None
+        session["user"] = ""
+        return render_template("logIn.html", loggedIn=loggedIn, user=user)
+    else:
+        loggedIn=False
+        user=None
+        return render_template("logIn.html", loggedIn=loggedIn, user=user)
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if session.get("book_id") != None:
+        session["book_id"] = None
+    if request.method == "POST":
+        if session.get("user") is None:
+            session["user"] = ""
+            return render_template("logIn.html")
+        elif session.get("user") == "":
+            return render_template("logIn.html")
+    return render_template("search.html")
+
+
+@app.route("/bookPage", methods=["POST"])
+def book():
+#search functionality
+    isbnNum = request.form.get("isbn")
+    bookAuth = request.form.get("author")
+    bookTitle = request.form.get("title")
+    if isbnNum != "":
+        try:
+            book = db.execute("SELECT * FROM books WHERE books.isbn=CAST(:isbnNum AS VARCHAR)", {"isbnNum":isbnNum}).fetchall()
+            if book is None:
+                    return render_template("notFound.html")
+            bookTitle = db.execute("SELECT title FROM books WHERE books.isbn=CAST(:isbnNum AS VARCHAR)", {"isbnNum":isbnNum}).fetchone()[0]
+            bookAuthor = db.execute("SELECT author FROM books WHERE books.isbn=CAST(:isbnNum AS VARCHAR)", {"isbnNum":isbnNum}).fetchone()[0]
+        except:
+            return render_template("error.html")
+
+    elif bookAuth != "" and bookTitle != "":
+        try:
+            book = db.execute("SELECT * FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchall()
+            if book is None:
+                return render_template("notFound.html")
+            isbnNum = db.execute("SELECT isbn FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchone()[0]
+            bookAuthor = db.execute("SELECT author FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchone()[0]
+        except:
+            return render_template("error.html")
+    elif bookTitle != "":
+        try:
+            book = db.execute("SELECT * FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchall()
+            if book is None:
+                return render_template("notFound.html")
+            isbnNum = db.execute("SELECT isbn FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchone()[0]
+            bookAuthor = db.execute("SELECT author FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchone()[0]
+        except:
+            return render_template("error.html")
+    elif bookAuth!= "":
+        try:
+            book = db.execute("SELECT * FROM books WHERE books.author=CAST(:bookAuth AS VARCHAR)", {"bookAuth":bookAuth}).fetchall()
+            if book is None:
+                return render_template("notFound.html")
+            else:
+                return render_template("manyResults.html", books=book)
+        except:
+            return render_template("error.html")
+
+    else:
+        return render_template("notFound.html")
+    book_id = db.execute("SELECT id FROM books WHERE books.title=CAST(:bookTitle AS VARCHAR)", {"bookTitle":bookTitle}).fetchone()[0]
+    session["book_id"] = book_id
+    print("The session book title:" + str(session["book_id"]))
+    return render_template("bookPage.html", bookTitle=bookTitle, bookAuthor=bookAuthor, isbnNum=isbnNum)
+
+@app.route("/reviewed", methods=["POST"])
+def postReview():
+
+    numberReview = request.form.get("rating")
+    textReview = request.form.get("textReview")
+    user_id = session.get("user")
+    book_id = session.get("book_id")
+    print(f"numberReview:{numberReview}\n textReview:{textReview}\n user_id:{user_id}\n book_id:{book_id}")
+    try:
+        db.execute("INSERT INTO reviews (user_id, book_id, score, textreview) VALUES (:user_id , :book_id, :score, :textreview)", {"user_id":user_id, "book_id":book_id, "score":numberReview, "textreview":textReview})
+        return render_template("postReview.html")
+        db.commit()
+    except:
+        return render_template("error.html")
